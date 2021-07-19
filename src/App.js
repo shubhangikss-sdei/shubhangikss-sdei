51eedcd790edc0df1b788c01959b0451df3c2b00
shubhangikss-sdei <87055138+shubhangikss-sdei@users.noreply.github.com>
2021-07-19 21:31:21 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import {Header} from './components/Header';
+import {Menu} from './components/Menu';
+import {Dashboard} from './components/Dashboard';
+import {Footer} from './components/Footer';
+function App() {
+  return (
+    <div className="wrapper">
+      
+      <Header />
+      <Menu />
+      <Dashboard />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
